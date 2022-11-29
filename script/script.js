0fea02e5704bec183c860a0b56be738dca9717a6
pescar <pescar@NBR666146.rio.ar.bsch>
2022-11-29 16:52:37 -0300

    Ultimo cambio

--- script/script.js
+++ script/script.js
@@ -1,18 +1,18 @@
 
 let slider = document.querySelector(".slider");
 
-let imagenes  =["image/cursos/argentina-programa.jpg","image/cursos/coder.jpg","image/cursos/educaciónIT.jpg","image/cursos/eggProgramacion.jpg","image/cursos/freeCodeCamp.png"]; 
+let imagenes  =["image/cursos/pescar.png","image/cursos/argentina-programa.jpg","image/cursos/coder.jpg","image/cursos/educaciónIT.jpg","image/cursos/eggProgramacion.jpg","image/cursos/freeCodeCamp.png"]; 
 let indices = 0;
 
 slider.src = imagenes[indices]
 
  cambio = ()=>{
-    if(indices <= 4){
+    if(indices <= 5){
         slider.src = imagenes[indices]
         indices++ 
     }else{
         indices = 0;
     }
 }
-setInterval(cambio, 4000);
+setInterval(cambio, 2000);
 //CARTA
